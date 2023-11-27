1015d4a21cdc54487773938c282151a694dd84fa
Ntirandekura Tharcisse <119756165+tharcissentirandekura@users.noreply.github.com>
2023-11-27 02:22:15 +0000

    saving the template

--- tharcisse/portfolio/views.py
+++ tharcisse/portfolio/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'tharcisse/home.html')
+    return render(request,'portfolio/home.html')
